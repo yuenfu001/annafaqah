ddbc1a54cf8886d14e748ab4f095d119ba95245f
yuenfu001 <64754712+yuenfu001@users.noreply.github.com>
2020-09-08 15:52:41 +0100

    added static images, template from html.design

--- anna/models.py
+++ anna/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class NewUser(models.Model):
+    first_name = models.CharField(max_length=20, null=True, help_text="your name")
+    last_name = models.CharField(max_length=20, null=True, help_text="your surname")
+    phone_number = models.CharField(
+        max_length=20, null=True, help_text="enter your mobile number here"
+    )
+    email = models.CharField(max_length=20, null=True, help_text="enter your Email")
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
